748e92dab6e3b2fda2015a7b6b34eabbb9115bfc
Tore Brandtzæg <toreb27@gmail.com>
2012-04-10 13:57:51 +0200

    Changes

--- pj600-sla-dashboard/Default.aspx.cs
+++ pj600-sla-dashboard/Default.aspx.cs
@@ -4,63 +4,169 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Data.OleDb;
 using System.Data;
-using no.nith.pj600.dashboard.Code;
+
 using log4net;
+using no.nith.pj600.dashboard.Code;
 
 namespace no.nith.pj600.dashboard
 {
    public partial class _Default : System.Web.UI.Page
    {
       private static readonly ILog log = LogManager.GetLogger(typeof(_Default));
-
-      private const string xlsMIME = "application/vnd.ms-excel";
-      private const string xlsxMIME = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+      private DatabaseClassesDataContext dataContext;
 
       protected void Page_Load(object sender, EventArgs e)
       {
-         
-        DataSet ds = FileHandler.GetDataSetFromExcel("Excel.xlsx");
-
-        if (ds != null)
-        {
-           GridView1.DataSource = ds.Tables[0].DefaultView;
-           GridView1.DataBind();
-        }
+         //Loads the Overview tab when the page loads, if it's not a postback.
+         if (!Page.IsPostBack)
+         {
+            ViewState["sortExpression"] = "";
+            ViewState["sortOrder"] = "";
+            LoadOverviewTab("", "");
+         }
+         else
+         {
+            LoadActiveTab("", "");
+         }
       }
 
-      protected void ExcelUploadButton_Click(object sender, EventArgs e)
+      protected void OnPageIndexChanging(object sender, EventArgs e)
+      {
+         ((GridView)sender).PageIndex = ((GridViewPageEventArgs)e).NewPageIndex;
+      }
+
+      protected void OnPageIndexChanged(object sender, EventArgs e)
+      {
+         ((GridView)sender).DataBind();
+      }
+
+      protected void TabContainerTabChange(object sender, EventArgs e)
+      {
+         LoadActiveTab("", "");
+      }
+
+      private void LoadActiveTab(string sortExpression, string sortOrder)
+      {
+         if (TabContainer.ActiveTab.ID.Equals("OverviewTab"))
+         {
+            LoadOverviewTab(sortExpression, sortOrder);
+         }
+         else if (TabContainer.ActiveTab.ID.Equals("SLATab"))
+         {
+            LoadSLATab(sortExpression, sortOrder);
+         }
+         else if (TabContainer.ActiveTab.ID.Equals("AddlServicesTab"))
+         {
+            LoadAddlServicesTab(sortExpression, sortOrder);
+         }
+         else
+         {
+            LoadGraphsTab(sortExpression, sortOrder);
+         }
+      }
+
+      private void LoadOverviewTab(string sortExpression, string sortOrder)
+      {
+         /*dataContext = new DatabaseClassesDataContext();
+         var query = from Customer in dataContext.Customers 
+                     where Customer.Name != null
+                     select Customer;
+         
+         OverviewTable.DataSource = query;
+         OverviewTable.DataBind();*/
+      }
+
+      private void LoadSLATab(string sortExpression, string sortOrder)
+      {
+         dataContext = new DatabaseClassesDataContext();
+
+         var query = (from c in dataContext.Customers
+                      join p in dataContext.Projects on c.CustomerNo equals p.CustomerNo
+                      join slaProjects in dataContext.SLAProjects on p.ProjectNo equals slaProjects.ProjectNo
+                      orderby p.ProjectNo
+                      select new { CustomerName = c.Name, ProjectNo = p.ProjectNo, ProjectName = p.Name }).Distinct();
+
+         //Creates a new DataTable and fills it with the rows from the query
+         DataTable dt = new DataTable();
+         dt.Columns.Add("CustomerName");
+         dt.Columns.Add("ProjectNo");
+         dt.Columns.Add("ProjectName");
+
+         foreach (var row in query)
+         {
+            DataRow newRow = dt.NewRow();
+            newRow["CustomerName"] = row.CustomerName;
+            newRow["ProjectNo"] = row.ProjectNo;
+            newRow["ProjectName"] = row.ProjectName;
+            dt.Rows.Add(newRow);
+         }
+
+         //Gets the DefaultView from the DataTable and sorts it if the sort expression isn't empty
+         DataView dv = dt.DefaultView;
+         if (sortExpression != string.Empty)
+         {
+            dv.Sort = sortExpression + " " + sortOrder;
+         }
+         else
+         {
+            dv.Sort = "ProjectNo asc";
+         }
+
+         //Sets and binds the GridViews data
+         SLATable.DataSource = dv;
+         SLATable.DataBind();
+      }
+
+      private void LoadAddlServicesTab(string sortExpression, string sortOrder)
       {
 
-         if (ExcelFileUpload.HasFile)
+      }
+
+      private void LoadGraphsTab(string sortExpression, string sortOrder)
+      {
+
+      }
+
+      protected void SLATable_OnSorting(object sender, GridViewSortEventArgs e)
+      {
+         SortExpression = e.SortExpression;
+         LoadActiveTab(e.SortExpression, SortOrder);
+
+      }
+
+      public string SortOrder
+      {
+         get
          {
-            if (ExcelFileUpload.PostedFile.ContentType == xlsMIME || ExcelFileUpload.PostedFile.ContentType == xlsxMIME)
+            if (ViewState["sortOrder"].Equals("desc"))
             {
-               ExcelFileUpload.SaveAs(Server.MapPath("~/App_Data/Excel.xlsx"));
-
-               ExcelUploadStatusLabel.CssClass = "infoMessage";
-               ExcelUploadStatusLabel.Text = "File successfully uploaded.";
-
-               log.Info("A new Excel data source has been uploaded.");
-
-
-               DataSet ds = FileHandler.GetDataSetFromExcel("Excel.xlsx");
-               if (ds != null)
-               {
-                  GridView1.DataSource = ds.Tables[0].DefaultView;
-                  GridView1.DataBind();
-               }
+               ViewState["sortOrder"] = "asc";
             }
-            else //Not an Excel file
+            else
             {
-               ExcelUploadStatusLabel.CssClass = "errorMessage";
-               ExcelUploadStatusLabel.Text = "The specified file is not an Excel file.";
+               ViewState["sortOrder"] = "desc";
             }
+
+            return ViewState["sortOrder"].ToString();
          }
-         else //No file selected
+         set
          {
-            ExcelUploadStatusLabel.CssClass = "errorMessage";
-            ExcelUploadStatusLabel.Text = "You need to specify a file to upload.";
+            ViewState["sortOrder"] = value;
+         }
+      }
+
+      public string SortExpression
+      {
+         get
+         {
+            return ViewState["sortExpression"].ToString();
+         }
+         set
+         {
+            ViewState["sortExpression"] = value;
          }
       }
    }
