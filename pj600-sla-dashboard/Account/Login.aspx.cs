ce1b407f0216b5a63143304fed03ef88814b902d
Tore Brandtzæg <toreb27@gmail.com>
2012-05-19 02:37:07 +0200

    OnAuthenticate in login

--- pj600-sla-dashboard/Account/Login.aspx.cs
+++ pj600-sla-dashboard/Account/Login.aspx.cs
@@ -43,6 +43,11 @@ namespace no.nith.pj600.dashboard.Account
          
       }
 
+      protected void OnAuthenticate(object sender, AuthenticateEventArgs e)
+      {
+         e.Authenticated = true;
+      }
+
       /*
        *  Method that's called after the LoginError event is fired from the login server control,
        *  when a login error is detected.
